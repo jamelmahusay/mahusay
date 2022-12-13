ac3897b95676e8d7c6187501a075c3ae2711a64e
the-name-romel <racopra@sorsu.edu.ph>
2022-12-13 11:38:49 +0800

    add contact me function

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,5 +1,6 @@
 import { Typewriter } from "react-simple-typewriter";
 import { profile } from "../assets/img";
+import { Link } from "react-scroll";
 
 export const Hero = () => {
   return (
@@ -21,8 +22,20 @@ export const Hero = () => {
                 />
               </span>
             </h2>
-            <button className="border-none bg-primary rounded-[8px] text-white text-[18px] px-[28px] py-[16px] mt-[30px] hover:bg-opacity-80">
-              Contact me
+            <button>
+              <Link
+                type="button"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-43}
+                duration={300}
+                delay={500}
+                isDynamic={true}
+                className="border-none bg-primary rounded-[8px] text-white text-[18px] px-[28px] py-[16px] mt-[30px] hover:bg-opacity-80"
+              >
+                Contact me
+              </Link>
             </button>
           </div>
 
